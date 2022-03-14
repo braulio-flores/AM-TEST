d84e92422c1542a654bef7656738e55739443db3
braulio flores <bfloresl1400@alumno.ipn.mx>
2022-03-14 15:35:44 -0600

    solo añadiendo un loader para la carga de personajes

--- src/components/characters/CharactersContainer.jsx
+++ src/components/characters/CharactersContainer.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { startSetCharacters } from "../../actions/characters";
 import CharacterCard from "./CharacterCard";
+import Swal from "sweetalert2";
 
 const CharactersContainer = () => {
   const { filter } = useSelector((state) => state.filter);
@@ -10,7 +11,19 @@ const CharactersContainer = () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(startSetCharacters(filter));
+    Swal.fire({
+      title: "Cargando Personajes",
+      text: "Por favor espere...",
+      allowEscapeKey: false,
+      showConfirmButton: false,
+      allowOutsideClick: false,
+
+      onBeforeOpen: () => {
+        Swal.showLoading();
+      },
+    });
+      dispatch(startSetCharacters(filter));
+    Swal.close();
   }, [filter, dispatch]);
 
   return (
